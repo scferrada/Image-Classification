614d8b4f0239b6d0437a26d2ab21a444305825fe
Sebastian Ferrada <scferradaa@gmail.com>
2015-05-31 19:40:12 -0300

    L curve

--- l_curve_for_sift.py
+++ l_curve_for_sift.py
@@ -3,22 +3,26 @@ import numpy as np
 import os
 import sys
 import matplotlib.pyplot as plt
+from VectorSerializer import VectorSerializer
 
 folder = sys.argv[1] #check if folder
 
 sample = []
 for dir, subdirlist, filelist in os.walk(folder):
 	for file in [f for f in filelist if not f.endswith('vindex.npy')]:
-		with VectorSerializer(os.path.join(folder,file)) as serializer:
+		with VectorSerializer(os.path.join(dir,file)) as serializer:
 			for i in xrange(serializer.size()):
-				sample.append(serializer.get(i))#=read descriptors from disk
-sample = np.float32(sample)
-
+				sample.extend(serializer.get(i))#=read descriptors from disk
+#sample = np.float32(sample)
+print len(sample)
+sample = np.array(sample)
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 0.1)
 wss = []
-for k in range(2, 100): #this is only to determine
+for k in xrange(10, 200, 5): #this is only to determine
 	compactness, labels, centers = cv2.kmeans(sample, k, criteria, 1, cv2.KMEANS_PP_CENTERS) #maybe more attempts are required, watch for memory limit
 	wss.append(compactness)
+	if k%10 == 0:
+		print k
 	
-plt.plot(wss, range(1,100), '-ro')
+plt.plot(range(10,200,5), wss, '-ro')
 plt.show()
