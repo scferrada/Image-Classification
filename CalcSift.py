24e38362177bd973f374f02ba58934cc377e0b01
Sebastian Ferrada <scferradaa@gmail.com>
2015-06-01 17:59:06 -0300

    ok

--- CalcSift.py
+++ CalcSift.py
@@ -4,17 +4,18 @@ import os
 import sys
 from VectorSerializer import VectorSerializer
 
-def calc_sift(img_path):
+def calc_sift(sift, img_path):
 	image = cv2.imread(img_path)
 	gray_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 	return sift.detectAndCompute(gray_img, None)
 
-folder = sys.argv[1] #check if folder
-outFolder = sys.argv[2]
-sift = cv2.SIFT()
-image_files = [f for f in os.listdir(folder) if os.path.isfile(os.path.join(folder,f))]
-for fileName in image_files:
-	filePath = os.path.join(folder,fileName)
-	kp, desc = calc_sift(filePath)
-	with VectorSerializer(os.path.join(outFolder,fileName)) as serializer:
-		serializer.append(desc)
+if __name__ == '__main__':	
+	folder = sys.argv[1] #check if folder
+	outFolder = sys.argv[2]
+	sift = cv2.SIFT()
+	image_files = [f for f in os.listdir(folder) if os.path.isfile(os.path.join(folder,f))]
+	for fileName in image_files:
+		filePath = os.path.join(folder,fileName)
+		kp, desc = calc_sift(sift, filePath)
+		with VectorSerializer(os.path.join(outFolder,fileName)) as serializer:
+			serializer.append(desc)
--- Classifier.py
+++ Classifier.py
@@ -1,7 +1,8 @@
 import cv2, os, sys
 import numpy as np
 from sklearn import svm
-from CalcSift import calc_sift
+import CalcSift as cs
+from VectorSerializer import VectorSerializer
 
 def print_usage():
 	print "Usage: python %s <kernel_args> <bovw_folder> <img_folder1> <class_name1> [<img_folder2> <class_name2>...]" % sys.argv[0]
@@ -21,7 +22,7 @@ def closest_center(x, centers):
 	return closer
 
 
-if len(sys.argv < 4):
+if len(sys.argv) < 4:
 	print_usage()
 
 
@@ -39,7 +40,8 @@ elif sys.argv[1].startswith("rbf"):
 		exit(1)
 else:
 	print_usage()
-	
+
+print "Reading training data"	
 bovw_folder = sys.argv[2]
 bovws = []
 classes = []
@@ -49,11 +51,13 @@ with VectorSerializer(os.path.join(bovw_folder,'out')) as serializer:
 		bovws.append(bovw)
 		classes.append(clazz)
 
-
+print "training..."
 clf.fit(bovws, classes)
 centers = 0
 idf = 0
 class_name1 = 0
+
+print "Reading cluster data"
 with VectorSerializer(os.path.join(bovw_folder, 'out.center')) as serializer:
 	centers = serializer.get(0)
 
@@ -62,21 +66,24 @@ with VectorSerializer(os.path.join(bovw_folder, 'out.idf')) as serializer:
 
 with VectorSerializer(os.path.join(bovw_folder, 'out.dict')) as serializer:
 	class_dict = serializer.get(0)
-
+print class_dict
+print "Calculating sift and predicting"
 folder = 3
 results = []
+sift = cv2.SIFT()
 for i in xrange(folder, len(sys.argv), 2):
 	img_folder = sys.argv[i]
 	img_class = sys.argv[i+1]
+	print img_folder
 	for file in [f for f in os.listdir(img_folder) if f.endswith('jpg')]:
-		kp, desc = calc_sift(file)
+		kp, desc = cs.calc_sift(sift, os.path.join(img_folder,file))
 		bovw = [0] * len(centers)
 		for x in desc:
 			center = closest_center(x, centers)
 			bovw[center] += 1
 		bovw = [1.0*x/len(kp) *idf[idx] for idx, x in enumerate(bovw)]
 		predicted_class = clf.predict(bovw)
-		results.append((class_dict.index(img_class), predicted_class))
+		results.append((class_dict[img_class], predicted_class))
 
 with VectorSerializer(os.path.join('.', 'results')) as serializer:
 	serializer.append(results)
